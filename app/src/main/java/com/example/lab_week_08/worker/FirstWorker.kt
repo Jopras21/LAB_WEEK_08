b492bab06824377282d6b9c6dd6bbaf54caf1a3c
Jopras21 <sun.phin098@gmail.com>
2025-10-30 14:36:29 +0700

    Commit No. 1 – add first & second worker

---
+++ app/src/main/java/com/example/lab_week_08/worker/FirstWorker.kt
@@ -0,0 +1,33 @@
+package com.example.lab_week_08.worker
+
+import android.content.Context
+import androidx.work.Data
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+
+class FirstWorker(context: Context, workerParams: WorkerParameters) : Worker(context, workerParams) {
+
+    // Fungsi ini menjalankan proses yang telah ditentukan berdasarkan input
+    // dan mengembalikan output setelah selesai
+    override fun doWork(): Result {
+        // Mendapatkan parameter input
+        val id = inputData.getString(INPUT_DATA_ID)
+
+        // Jeda proses selama 3 detik
+        Thread.sleep(3000L)
+
+        // Membangun output berdasarkan hasil proses
+        val outputData = Data.Builder()
+            .putString(OUTPUT_DATA_ID, id)
+            .build()
+
+        // Mengembalikan output
+        return Result.success(outputData)
+    }
+
+    // Object pendamping untuk menyimpan konstanta
+    companion object {
+        const val INPUT_DATA_ID = "inId"
+        const val OUTPUT_DATA_ID = "outId"
+    }
+}
