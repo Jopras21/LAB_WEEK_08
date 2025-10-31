6a7eec3dbb25a726097f3f54812137623e81fbed
Jopras21 <sun.phin098@gmail.com>
2025-10-31 09:16:44 +0700

    Commit No. 2 – add first foreground service

--- app/src/main/java/com/example/lab_week_08/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_08/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.example.lab_week_08
 
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.work.Constraints
@@ -31,6 +35,12 @@ class MainActivity : AppCompatActivity() {
                 systemBars.bottom)
             insets
         }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (checkSelfPermission(android.Manifest.permission.POST_NOTIFICATIONS) !=
+                PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(arrayOf(android.Manifest.permission.POST_NOTIFICATIONS), 1)
+            }
+        }
         //Create a constraint of which your workers are bound to.
         //Here the workers cannot execute the given process if
         //there's no internet connection
@@ -38,41 +48,41 @@ class MainActivity : AppCompatActivity() {
             .setRequiredNetworkType(NetworkType.CONNECTED)
             .build()
         val id = "001"
-//There are two types of work request:
-//OneTimeWorkRequest and PeriodicWorkRequest
-//OneTimeWorkRequest executes the request just once
-//PeriodicWorkRequest executed the request periodically
-//Create a one time work request that includes
-//all the constraints and inputs needed for the worker
-//This request is created for the FirstWorker class
+        //There are two types of work request:
+        //OneTimeWorkRequest and PeriodicWorkRequest
+        //OneTimeWorkRequest executes the request just once
+        //PeriodicWorkRequest executed the request periodically
+        //Create a one time work request that includes
+        //all the constraints and inputs needed for the worker
+        //This request is created for the FirstWorker class
         val firstRequest = OneTimeWorkRequest
             .Builder(FirstWorker::class.java)
             .setConstraints(networkConstraints)
             .setInputData(getIdInputData(FirstWorker
                 .INPUT_DATA_ID, id)
             ).build()
-//This request is created for the SecondWorker class
+        //This request is created for the SecondWorker class
         val secondRequest = OneTimeWorkRequest
             .Builder(SecondWorker::class.java)
             .setConstraints(networkConstraints)
             .setInputData(getIdInputData(SecondWorker
                 .INPUT_DATA_ID, id)
             ).build()
-//Sets up the process sequence from the work manager instance
-//Here it starts with FirstWorker, then SecondWorker
+        //Sets up the process sequence from the work manager instance
+        //Here it starts with FirstWorker, then SecondWorker
         workManager.beginWith(firstRequest)
             .then(secondRequest)
             .enqueue()
-//All that's left to do is getting the output
-//Here, we receive the output and displaying the result as a toast message
-//You may notice the keyword "LiveData" and "observe"
-//LiveData is a data holder class in Android Jetpack
-//that's used to make a more reactive application
-//the reactive of it comes from the observe keyword,
-//which observes any data changes and immediately update the app UI
-//Here we're observing the returned LiveData and getting the
-//state result of the worker (Can be SUCCEEDED, FAILED, or CANCELLED)
-//isFinished is used to check if the state is either SUCCEEDED or FAILED
+        //All that's left to do is getting the output
+        //Here, we receive the output and displaying the result as a toast message
+        //You may notice the keyword "LiveData" and "observe"
+        //LiveData is a data holder class in Android Jetpack
+        //that's used to make a more reactive application
+        //the reactive of it comes from the observe keyword,
+        //which observes any data changes and immediately update the app UI
+        //Here we're observing the returned LiveData and getting the
+        //state result of the worker (Can be SUCCEEDED, FAILED, or CANCELLED)
+        //isFinished is used to check if the state is either SUCCEEDED or FAILED
         workManager.getWorkInfoByIdLiveData(firstRequest.id)
             .observe(this) { info ->
                 if (info.state.isFinished) {
@@ -83,6 +93,7 @@ class MainActivity : AppCompatActivity() {
             .observe(this) { info ->
                 if (info.state.isFinished) {
                     showResult("Second process is done")
+                    launchNotificationService()
                 }
             }
     }
@@ -94,5 +105,26 @@ class MainActivity : AppCompatActivity() {
     //Show the result as toast
     private fun showResult(message: String) {
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+    private fun launchNotificationService() {
+//Observe if the service process is done or not
+//If it is, show a toast with the channel ID in it
+        NotificationService.trackingCompletion.observe(
+            this) { Id ->
+            showResult("Process for Notification Channel ID $Id is done!")
+        }
+//Create an Intent to start the NotificationService
+//An ID of "001" is also passed as the notification channel ID
+        val serviceIntent = Intent(
+            this,
+            NotificationService::class.java
+        ).apply {
+            putExtra(EXTRA_ID, "001")
+        }
+//Start the foreground service through the Service Intent
+        ContextCompat.startForegroundService(this, serviceIntent)
+    }
+    companion object{
+        const val EXTRA_ID = "Id"
     }
 }
